4708e449e3b41e430eeec973b1171c4fc98d7ce8
정선우 <laborlawseon@gmail.com>
2023-03-01 22:22:52 +0900

    자바 소켓 통신 맛보기

--- schema.sql
+++ schema.sql
@@ -6,3 +6,8 @@ create table member (
 );
 insert into member(member_id, money) values ('hi1',10000);
 insert into member(member_id, money) values ('hi2',20000);
+
+-- AUTO COMMIT 끄기 --
+-- EDIT > PREFERENCES > SQL EXECUTION > NEW CONNECTIONS USE AUTO COMMIT MODE 체크해제 후 편집기에서 SELECT @@autocommit; 실행 후 0이 나오면 성공
+
+-- DB에서 락을 가지고 있어야 데이터를 수정할 수 있다.
