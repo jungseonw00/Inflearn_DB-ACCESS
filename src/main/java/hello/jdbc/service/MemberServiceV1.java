a5b70af944f4d90928936af6aed56f518a17a846
정선우 <laborlawseon@gmail.com>
2023-03-04 21:56:24 +0900

    트랜잭션 개념 정확히 이해하기

--- src/main/java/hello/jdbc/service/MemberServiceV1.java
+++ src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -12,14 +12,13 @@ public class MemberServiceV1 {
     private final MemberRepositoryV1 memberRepository;
 
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        // 트랜잭션 시작 (같은 커넥션 유지)
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
-
         memberRepository.update(fromId, fromMember.getMoney() - money);
-
         validation(toMember);
-
         memberRepository.update(toId, toMember.getMoney() + money);
+        // 트랜잭션 종료 (커밋 또는 롤백)
     }
 
     private static void validation(Member toMember) {
